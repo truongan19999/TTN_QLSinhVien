4cd6390263d8ccb248d4e13c02a9291bf6079f39
tamquy999 <tamquy999@gmail.com>
2020-12-21 09:14:39 +0700

    Them DAO GiaoVien

---
+++ QuanLyHocSinhTHPT/DAO/GiaoVienDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyHocSinhTHPT.DTO;
+
+namespace QuanLyHocSinhTHPT.DAO
+{
+    class GiaoVienDAO
+    {
+        private static GiaoVienDAO instance;
+
+        internal static GiaoVienDAO Instance
+        {
+            get { if (instance == null) instance = new GiaoVienDAO(); return instance; }
+            private set { instance = value; }
+        }
+    }
+}
